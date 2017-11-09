cdc40a943a22d1a9de118f2969cb4efde563af20
Chronosis <jayreardon@gmail.com>
2017-11-08 23:26:03 -0700

    Updating gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,17 @@
 const gulp = require('gulp');
-const notify = require('gulp-notify');        // Provide Gulp with a way to create push notifications
-const plumber = require('gulp-plumber');      // Handle Errors without breaking
-const eslint = require('gulp-eslint');        // ES6 JS/JSX Lineter -- Check for syntax errors
-const mocha = require('gulp-mocha');          // Test Framework
+// ES6 JS/JSX Lineter -- Check for syntax errors
+const eslint = require('gulp-eslint');
+// Test Framework
+const mocha = require('gulp-mocha');
 const config = require('./build.config');
 
 const devFolder = config.devFolder;
-const configFolder = config.configFolder;
-const docsFolder = config.docsFolder;
-
-// Route Errors to the Notificication Tray
-const onError = (err) => {
-  notify.onError({
-    title:    'Error',
-    message:  '<%= error %>',
-  })(err);
-  this.emit('end');
-};
-
-const plumberOptions = {
-  errorHandler: onError
-};
+// const configFolder = config.configFolder;
+// const docsFolder = config.docsFolder;
 
 // Lint JS Files
 gulp.task('lint', () => {
-  return gulp.src(devFolder + '/**/*.js')
+  return gulp.src(`${devFolder}/**/*.js`)
     .pipe(eslint({ configFile: '.eslintrc.json' }))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
@@ -33,13 +20,13 @@ gulp.task('lint', () => {
 gulp.task('test', ['lint'], () => {
   return gulp.src('test.js', { read: false })
     .pipe(mocha())
-    .once('error', function() {
+    .once('error', () => {
       process.exit(1);
     });
 });
 
 gulp.task('lintfix', () => {
-  return gulp.src(devFolder + '/**/*.js')
+  return gulp.src(`${devFolder}/**/*.js`)
     .pipe(eslint({ configFile: '.eslintrc.json', fix: true }))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
