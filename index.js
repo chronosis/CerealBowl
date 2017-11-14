e4a5b7d5c384e29076bc1639b2e6f5cbdc0de9f6
Jay Reardon <jayreardon@gmail.com>
2017-11-14 12:34:03 -0700

    Added logging support

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // index.js
 const errors = require('./lib/errors');
 const JObjStream = require('./lib/jObjStream');
+const Logger = require('./lib/logstub');
 
 class JDeserialize {
   constructor(log) {
-    this.log = log || null;
+    this.log = log || new Logger();
   }
 
   read(buff) {
     let obj = null;
     try {
       if (!(buff instanceof Buffer)) {
-        this._reportError(errors.BUFFER_REQUIRED);
+        this.log.error(errors.BUFFER_REQUIRED);
       } else {
         const objStream = new JObjStream(buff, this.log);
         obj = objStream._read();
       }
     } catch (err) {
-      console.log(err.stack || err);
+      this.log.debug(err.stack || err);
     }
     return obj;
   }
